af2c75b549c6a6a0e0dda79ee2916dfe5e823d16
Manoel Fernando <piloukos45@gmail.com>
2023-01-12 15:43:42 -0300

    ghgtsfr

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,19 @@
 
-const currentLocation = location.href;
-const menuItems = document.querySelectorAll('a');
-const menuLenght = menuItems.length;
+const currentLocation = document.querySelectorAll('nav a');
+const css_ativo ='ativo';
 
 
-for(let i =0; i < menuLenght; i++){
-    if(menuItems[i].href === currentLocation){
-         var cor = menuItems[i].className = "ativo";
-     
-    }
 
-  
-}
+currentLocation.forEach(function(e){
+    e.addEventListener('click', function(){
+        for(let i =0; i < currentLocation.length; i++){
+            currentLocation[i].classList.remove(css_ativo);
+        }
+        e.classList.add(css_ativo);
+    })
+})
+
+
 
 
 
